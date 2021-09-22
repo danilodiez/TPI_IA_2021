70a10930d54a07f512876a3816e757afd439666f
danilodiez <daniloo10.dd@gmail.com>
2021-09-22 16:56:26 -0300

    Add papaparse to read CSV

---
+++ src/scripts/tree.js
@@ -0,0 +1,51 @@
+/*
+TODO: FUNCION decisiontree(data, atributos, arbol)
+TODO:  si data contiene un solo Cj, una sola clase a predecir
+*:      hacer una hoja en T rotulada con Cj
+TODO:  sino si atributos=vacio
+*:      hacer una hoja en T rotulada con Cj
+TODO:  sino si data tiene muestras de varias clases, seleccionamos un solo atributo para particionar data en subsets
+TODO:
+TODO:    po = evaluamos impureza de todo el conjunto (data)
+TODO:    for cada atributo Ai de A
+TODO:      pi = evaluar impureza de cada Ai (Ai, data)
+TODO:
+TODO:    seleccionamos Ag con la mejor ganancia
+TODO:    if ganancia < threshold
+*:        hacer una hoja en T rotulada con Cj
+TODO:    sino
+TODO:      Ag va a ser un nodo decision en T
+TODO:      por cada valor de Ag particionamos D en m conjuntos
+TODO:
+TODO:      for each Dj particiones de data
+TODO:        creamos una rama con nodo decision para Tj como hijo de T
+TODO:
+TODO:        return decisiontree(Dj, A sin Ag, Tj)
+*/
+const fs = require('fs');
+const Papa = require('papaparse');
+const csvFilePath = '../data/drug200.csv'
+
+// Function to read csv which returns a promise so you can do async / await.
+
+const readCSVfromLocal = async (filePath) => {
+  const csvFile = fs.readFileSync(filePath)
+  const csvData = csvFile.toString()
+  return new Promise(resolve => {
+    Papa.parse(csvData, {
+      header: true,
+      complete: results => {
+        console.log('Complete', results.data.length, 'records.');
+        resolve(results.data);
+      }
+    });
+  });
+};
+
+const getCSV = async () => {
+  let parsedData = await readCSVfromLocal(csvFilePath);
+  return parsedData
+}
+
+
+getCSV().then(data => console.log(data))
