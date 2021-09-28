15063e74211be05466c95716153bcb65282e6c1f
danilodiez <daniloo10.dd@gmail.com>
2021-09-28 20:55:18 -0300

    First try on tree graphic with react d3 tree

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,45 @@
 import React, { useState } from 'react'
+import Tree from "react-d3-tree";
+import './App.css'
+const tree = {
+      name: "Argentina?",
+      attributes: {
+        department: "8/10",
+      },
+      children: [
+        {
+          name: `Resistencia?`,
+          attributes: {
+            atributo: "",
+          },
+          children: [
+            {
+              name: "Facha = si",
+            },
+          ],
+        },
+        {
+          name: "Corrientes?",
+          attributes: {
+            atributo: "",
+          },
+          children: [
+            {
+              name: "Facha = no",
+            },
+          ],
+        },
+      ],
+    }
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-
+      <Tree data={orgChart} orientation='vertical' />
     </div>
-  )
+  );
 }
 
 export default App
