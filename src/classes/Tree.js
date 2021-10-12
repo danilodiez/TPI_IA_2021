ea2f774fdb1d415eb7e44fd7663b2c65ef71b9d3
danilodiez <daniloo10.dd@gmail.com>
2021-10-11 22:23:53 -0300

    Add tree class changes

--- src/classes/Tree.js
+++ src/classes/Tree.js
@@ -1,12 +1,12 @@
-class Tree {
-  constructor(question, entropy, gain = 0, gainRatio=0, branches, isLeaf = false) {
-    this.question = question
+export default class Tree {
+  constructor(node = '', entropy = null, gain = null, gainRatio = null, branches = [], isLeaf = false, descendants=[]) {
+    this.node = node
     this.gainRatio = gainRatio
     this.entropy = entropy
     this.gain = gain
     this.branches = branches
     this.isLeaf = isLeaf
-    this.descendants = []
+    this.descendants = descendants
   }
 }
 
