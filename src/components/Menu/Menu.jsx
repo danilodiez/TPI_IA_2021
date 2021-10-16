33f143756e2107f85b4d1240ac2ebf50022aa4b8
selrouvier <selrouvier@gmail.com>
2021-10-16 13:03:59 -0300

    add structure and styles of menu component

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
+import "./styles.css";
 
 const Menu = () => {
  
   return (
-    <div>
-      <h1>Menu</h1>
+    <div className="container-menu">
+      <div className="pt-4">
+        <h4 className="text-center p-4">
+          <button onClick={() => {history.push("/index")}}>
+            Menu Principal
+          </button>
+        </h4>
+      </div>
+      <div className="p-4">
+        <ul className="list-menu">
+          <li class="m-2 color-white">
+          <button onClick={() => {history.push("/tutorial")}}>
+              Tutorial
+            </button>
+          </li>
+          <li class="m-2">
+          <button onClick={() => {history.push("/tree")}}>
+              Arbol de decisión
+            </button>
+          </li>
+          <li class="m-2">
+          <button onClick={() => {history.push("/samples")}}>
+              Muestras
+            </button>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
