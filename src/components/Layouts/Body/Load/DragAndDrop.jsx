d1d447a5e76ae66ae2a1c6afbbf23920f6b9911c
danilodiez <daniloo10.dd@gmail.com>
2021-10-19 19:23:42 -0300

    Add drag and drop component

--- src/components/Layouts/Body/Load/DragAndDrop.jsx
+++ src/components/Layouts/Body/Load/DragAndDrop.jsx
@@ -1,32 +1,50 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import { FileUploader } from "react-drag-drop-files";
 import "bootstrap/dist/css/bootstrap.min.css";
+import fileImage from "../../../../assets/images/fileImage.png";
 
 const fileTypes = ["TXT", "CSV"];
 
 const boxBody = () => {
-  return(
-    <div>
-      Suba su archivo
+  return (
+    <div className="justify-content-center align-items-center d-flex flex-column">
+      <span>Arrastre aqui sus archivos o haga click en el recuadro</span>
+      <img
+        src={fileImage}
+        width="50"
+        height="50"
+        alt="Logo de archivo"
+      />
     </div>
-  )
-}
+  );
+};
 const DragAndDrop = () => {
   const [file, setFile] = useState(null);
+  const [success, setSuccess] = useState(false)
   const handleChange = (file) => {
     setFile(file);
-    console.log(file)
-
+    setSuccess(true)
   };
   return (
-    <div class="h-100 border border-danger justify-content-center d-flex">
+    <div className="h-100 justify-content-center d-flex align-items-center flex-column">
       <FileUploader
         handleChange={handleChange}
         name="file"
         types={fileTypes}
-        classes="border border-info rounded-10 h-100 w-100 d-flex justify-content-center"
+        children={boxBody()}
+        maxSize={2}
+        classes="border border-info rounded-10 h-25 w-100 d-flex justify-content-center"
+        onSizeError={() => {
+          //TODO ACA TRIGGEREAMOS EL MODAL
+          setSuccess(false);
+        }}
+        onTypeError={() => {
+          //TODO ACA TRIGGEREAMOS EL MODAL
+          setSuccess(false);
+        }}
       />
+      {success ? <> ✅ El archivo se subio con exito</> : undefined}
     </div>
   );
-}
-export default DragAndDrop
+};
+export default DragAndDrop;
